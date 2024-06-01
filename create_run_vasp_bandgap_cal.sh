a8c3133138e1d5a416434b8e68c89706cf42ec39
AkiraTOSEI <akihiro.fujii0137@gmail.com>
2024-06-01 15:41:22 +0900

    MAGMOMの値をベースに計算するように修正

--- create_run_vasp_bandgap_cal.sh
+++ create_run_vasp_bandgap_cal.sh
@@ -28,7 +28,7 @@ NPROCS=\$(cat \$PBS_NODEFILE | wc -l)
 
 # 最終計算用のディレクトリを作り、必要なファイルを移動させる。
 mkdir -p bandgap_cal
-cp POTCAR KPOINTS calculate_bandgap.py ./bandgap_cal
+cp POTCAR calculate_bandgap.py ./bandgap_cal
 cd bandgap_cal
 
 echo "################################################"
@@ -50,12 +50,12 @@ echo "0 0 0" >> KPOINTS
 
 # ENCUTの収束後値をもとに、bandgap計算用のINCARを作成する.
 BEST_ENCUT=\$(cat ../BEST_ENCUT.dat)
+EDIFF=\$(cat ../EDIFF.dat)
 echo "bandap calculation INCAR" > INCAR
 echo "ISTART = 1 #default:1, 0:scratch, 1:WAVECARを読む、なければ0になる " >> INCAR
 echo "ICHARG = 11 #default:2(ISTART=0)0,else:0, 0:初期波動関数から電荷を計算,2:原子電荷の重ね合わせを使う,11:CHGCAR利用" >> INCAR
 echo "ISPIN = 2 # default:1, 1:スピン分極なし、2:スピン分極あり" >> INCAR
 echo "\$BEST_ENCUT" >> INCAR
-echo "EDIFF = 1.0e-6 # default:10^-4, SCF計算の収束条件,推奨は1E-6らしい。" >> INCAR
 echo "LWAVE = .F. # WAVECARを出力するか" >> INCAR
 echo "ISMEAR = -5 # default:1,部分占有軌道の設定、絶縁体・半導体では-5を推奨 " >> INCAR
 echo "#SIGMA = 0.01 # default:0.2, smearingの幅、系の事前知識がないときはISMEAR=0でSIGMA=0.03~0.05 " >> INCAR
@@ -65,7 +65,9 @@ echo "NELM = 200 # default:60, SC計算の最大回数を指定" >> INCAR
 #echo "NPAR = 4 # defalut:コア数、並列計算に使うコア数" >> INCAR
 echo "LORBIT = 11 # default: None, 11:DOSCAR, PROCARを出力" >> INCAR
 echo "NEDOS = 2000 #default:301, DOS計算のグリッド数" >> INCAR
+echo \$EDIFF >> INCAR
 cat ../INCAR_tail >> INCAR
+cat ../incar_magmom.dat >> INCAR
 
 mpiexec -iface ib0 -launcher rsh -machinefile \$PBS_NODEFILE -ppn 16 /home/share/VASP/vasp.5.4.4
 
@@ -105,9 +107,9 @@ echo "#######################################"
 cd ../
 
 # 特定のファイル以外を削除
-rm -rf bandgap_cal final_sc relax_cal
-shopt -s extglob
-rm -f !(POSCAR_gt|POSCAR_distorted|final_INCAR|final_KPOINTS|final_OUTCAR|final_DOSCAR|final_POSCAR|used_POTCAR.txt|kp_history.dat|encut_history.dat)
+#rm -rf bandgap_cal final_sc relax_cal
+#shopt -s extglob
+#rm -f !(POSCAR_gt|POSCAR_distorted|final_INCAR|final_KPOINTS|final_OUTCAR|final_DOSCAR|final_POSCAR|used_POTCAR.txt|kp_history.dat|encut_history.dat)
 
 date
 
