096c0d35ebc10a2429dd80a27e5577549c417956
AkiraTOSEI <akihiro.fujii0137@gmail.com>
2024-06-01 20:45:41 +0900

    同じ原子が複数個あるときのバグに対応

--- magmom.py
+++ magmom.py
@@ -4,16 +4,18 @@ def process_poscar(file_path):
         
     # Read the 6th line (index 5)
     atoms_line = lines[5].strip().split()
+    num_line = lines[6].strip().split()
     
     print("MAGMOM =", end="")
     
     # Process each atom
-    for atom in atoms_line:
+    for atom, num  in zip(atoms_line, num_line):
         if atom in ['Fe', 'Co', 'Ni', 'Gd']:
             mag_val = 1.0
         else:
             mag_val = 0.0
-        print(f" {mag_val}", end="")
+        for _ in range(int(num)):
+            print(f" {mag_val}", end="")
     
     print("")
 
